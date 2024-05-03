da25434c967f30f57b88b010f30a817867acda70
Daniel Salinas <zzorba@users.noreply.github.com>
2024-05-03 15:53:12 -0400

    Initial push with reproduction

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,38 @@
-import { StyleSheet } from 'react-native';
+import { NativeModules, StyleSheet, TextInput } from 'react-native';
+import { Keyboard } from 'react-native-ui-lib'
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { useCallback, useRef } from 'react';
 
 export default function TabOneScreen() {
+  const component = useRef<TextInput | null>();
+  const renderContent = useCallback(() => {
+    return (
+      <View style={{ backgroundColor: 'red', minHeight: 40 }}>
+        <TextInput
+          ref={(node) => {
+            component.current = node;
+          }}
+          multiline
+          placeholder='Foo'
+          style={{ color: 'white', fontSize: 40 }}
+        />
+      </View>
+    );
+  }, []);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Keyboard.KeyboardAccessoryView
+        renderContent={renderContent}
+        kbInputRef={component}
+        addBottomView
+        usesBottomTabs
+        scrollBehavior={NativeModules.KeyboardTrackingViewTempManager?.KeyboardTrackingScrollBehaviorFixedOffset}
+      />
     </View>
   );
 }
